e12c6b29c03fe553961c195929ce086aca8b42e4
Teo Sartori <m@sartori.dk>
2015-12-18 14:11:31 +0100

    Add address resolution from mDNS.
    It still relies on the IPFS node advertising itself as _ipfs._tcp.
    
    License: MIT
    Signed-off-by: Teo Sartori <m@sartori.dk>

--- IpfsIosApp/ViewController.swift
+++ IpfsIosApp/ViewController.swift
@@ -68,12 +68,14 @@ class ViewController: UIViewController {
  Service: <NSNetService 0x7fd8438005a0> . ipfs.local. discovery
  ...
 */
-class IpfsNodeDiscovery : NSObject, NSNetServiceBrowserDelegate {
+class IpfsNodeDiscovery : NSObject, NSNetServiceBrowserDelegate, NSNetServiceDelegate {
     let DOMAIN = "" //"local"
 //    let SERVICE_TYPE = "_services._dns-sd._udp."//"_airplay._tcp."
-    let SERVICE_TYPE = "_discovery._ipfs."
+//    let SERVICE_TYPE = "_discovery._ipfs."
+        let SERVICE_TYPE = "_ipfs._tcp."
 //    let SERVICE_TYPE = "_1password4._tcp."
     let domainBrowser: NSNetServiceBrowser
+    var active_service: NSNetService?
     
     override init() {
         domainBrowser = NSNetServiceBrowser()
@@ -87,6 +89,18 @@ class IpfsNodeDiscovery : NSObject, NSNetServiceBrowserDelegate {
     
     func netServiceBrowser(browser: NSNetServiceBrowser, didFindService service: NSNetService, moreComing: Bool) {
         print("Service: \(service)")
+        print("name: \(service.name)")
+        print("type: \(service.type)")
+
+        if service.type == SERVICE_TYPE {
+//            if service.name == "_ipfs" {
+            // Store the service so we can be sure it's not released.
+
+            active_service = service
+            print(active_service!.hostName)
+            active_service!.delegate = self
+            active_service!.resolveWithTimeout(2.0)
+        }
     }
     
     func netServiceBrowserWillSearch(browser: NSNetServiceBrowser) {
@@ -94,6 +108,52 @@ class IpfsNodeDiscovery : NSObject, NSNetServiceBrowserDelegate {
     }
     func netServiceBrowserDidStopSearch(browser: NSNetServiceBrowser) {
         print("gone.")
+    }
+    
+    func netServiceDidResolveAddress(sender: NSNetService) {
+        print("Bingo \(sender.addresses)")
+        guard let addresses = sender.addresses else { return }
+        
+        enum Ip_socket_address {
+            case sa(sockaddr)
+            case ipv4(sockaddr_in)
+            case ipv6(sockaddr_in6)
+        }
+        
+        for addr in addresses {
+            var storage = sockaddr_storage()
+            addr.getBytes(&storage, length: sizeof(sockaddr_storage))
+            let buf = UnsafeMutablePointer<Int8>.alloc(Int(INET6_ADDRSTRLEN))
+            var ipc = UnsafePointer<Int8>()
+            
+            switch Int32(storage.ss_family){
+                
+            case AF_INET:
+                let addrData = withUnsafePointer(&storage) { UnsafePointer<sockaddr_in>($0).memory }
+                var addr = addrData.sin_addr
+                ipc = inet_ntop(Int32(addrData.sin_family), &addr, buf, __uint32_t(INET6_ADDRSTRLEN))
+                
+            case AF_INET6:
+                let addr6Data = withUnsafePointer(&storage) { UnsafePointer<sockaddr_in6>($0).memory }
+                var addr = addr6Data.sin6_addr
+                ipc = inet_ntop(Int32(addr6Data.sin6_family), &addr, buf, __uint32_t(INET6_ADDRSTRLEN))
+
+            default: break
+            }
+            print(String.fromCString(ipc))
+        }
+    }
+    
+    func netServiceWillResolve(sender: NSNetService) {
+        print("Resolving...")
+    }
+    
+    func netService(sender: NSNetService, didNotResolve errorDict: [String : NSNumber]) {
+        print("nohappy")
+    }
+    func netServiceDidStop(sender: NSNetService) {
+        print("stop")
+        active_service = nil
     }
 }
 
